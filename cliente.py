11787d63eaa5084ef2cedd335e9503bd6a92072f
Gustavo Santos <97034602+gustavosantos43@users.noreply.github.com>
2022-07-05 12:21:04 -0300

    Add files via upload

--- cliente.py
+++ cliente.py
@@ -1,36 +1,97 @@
-#!/usr/bin/env/python3
+#!/usr/bin/env python3
 import socket
 import sys
+import os
+import time
 
-HOST = 'localhost'     # Endereco IP do Servidor
-PORT = 40000          # Porta que o Servidor esta
+HOST = '127.0.0.1'     # Endereco IP do Servidor
+PORT = 40000           # Porta que o Servidor esta
 
+def flush_input():
+	try:
+		import sys, termios
+		termios.tcflush(sys.stdin, termios.TCIOFLUSH)
+	except ImportError:
+		import msvcrt
+		while msvcrt.kbhit():
+			msvcrt.getch()
+
+def limpar():
+	os.system('cls' if os.name == 'nt' else 'clear')
+ 
+def comando_usuario(cmd_usr):
+  cmd_map = {
+    'sair': 'quit',
+    'jogar': 'play',
+    'chutar' : 'kick',
+  }
+  
+  tokens = cmd_usr.split()
+  if tokens[0].lower() in cmd_map:
+    tokens[0] = cmd_map[tokens[0].lower()]
+    return " ".join(tokens)
+  else:
+    return False    
+    
 if len(sys.argv) > 1:
-    HOST = sys.argv[1]
-
+  HOST = sys.argv[1]
+  
 print('Servidor: ', (HOST, PORT))
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-dest = (HOST, PORT)
-tcp.connect(dest)
-print( '''
-  JOGO DA FORCA
-    +------+
-    |      |
-    |      O
-    |     /|\\
-    |     / \\
-       
-===========================
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serv = (HOST, PORT)
+sock.connect(serv)
 
-''')
-print ('Para sair do jogo use CTRL+C')
-while True:
-    try:
-        msg = input('Mensagem: ')
-    except: break
-    tcp.send(str.encode(msg))
-    msg = tcp.recv(1024)
-    if not msg: break
-    print ('Recebido: ', msg.decode())
+nome = input("Nome: ")
+time.sleep(0.001)
+sock.send(nome.encode())
 
-tcp.close()
+print('#############################################################')
+print('\t\t\tJOGO DA FORCA')
+print('#############################################################')
+print('Para começar o jogo digite "play"')
+print('Para encerrar o jogo digite "quit"')
+
+minhapalavra = sock.recv(1024)
+
+fim = 1;
+while(fim):
+  limpar()
+  placar = sock.recv(1024)
+  if(placar.startswith("fim de jogo".encode())):
+    break
+
+
+  print('\n'+placar.decode())
+  time.sleep(0.001)
+  sock.send('nada'.encode())
+
+  print("Sua palavra: \n")
+  for c in minhapalavra:  # printa sua a palavra
+    print("{}".format(c) + ' '),
+
+  print ('\n')
+
+  ret = sock.recv(1024)
+  if(ret.decode() == "É a sua vez de jogar: "):
+    time.sleep(0.001)
+    flush_input()
+    sock.send(input("\nEscolha uma letra ou chute a palavra: ".encode())).lower()
+    minhapalavra = sock.recv(1024)
+
+  else:
+    print("\n"+ret.decode())
+		
+    fimdeturno = sock.recv(1024)
+
+vencedores = []
+vencedores = placar.split('|')
+
+if(len(vencedores)== 2):
+	print("O jogo acabou, o vencedor foi "+vencedores[1]+".")
+elif(len(vencedores) > 2):
+	print("O jogo acabou empatado, os vencedores foram:")
+	for i in range(1, len(vencedores)):
+		print (vencedores[i])
+
+
+sock.close()
--- servidor.py
+++ servidor.py
@@ -1,30 +1,129 @@
-#!/usr/bin/env/python3
+#!/usr/bin/env python3
 import socket
-import os
+import os 
+import time
 
-HOST = ''              # Endereco IP do Servidor
-PORT = 40000          # Porta que o Servidor esta
+class jogador():
+    def __init__(self, nome, conn, ip):
+        self.nome = nome
+        self.conn = conn
+        self.ip = ip
+        self.pontos = 0
+        self.palavra = ''
 
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def limpar():
+	os.system('cls' if os.name == 'nt' else 'clear')
+
+#TRATAMENTO DO SOCKET
+HOST = '0.0.0.0'     # Endereco IP do Servidor
+PORT = 40000           # Porta que o Servidor escuta
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv = (HOST, PORT)
-tcp.bind(serv)
-tcp.listen(50)
-while True:
-    try:
-        con, cliente = tcp.accept()
-    except: break
-    pid = os.fork()
-    if pid == 0:
-        tcp.close()
-        print ('Conectado por', cliente)
-        while True:
-            msg = con.recv(1024)
-            if not msg: break
-            print ('Recebido: ', msg.decode())
-            con.send(msg)
-        print ('Finalizando conexao do cliente', cliente)
-        con.close()
-        break
-    else:
-      con.close()
-tcp.close()
+sock.bind(serv)
+sock.listen(50)
+
+num_jogadores = int(input("Quantidade de jogadores: "))
+
+lista_jogadores = []
+lista_vencedores = []
+
+def checarpontos(i):
+    if(lista_jogadores[i].pontos <= 0):
+        lista_jogadores.pop(i)
+
+#ESPERANDO OS JOGADORES ENTRAREM NO JOGO
+for i in range(num_jogadores):
+    conn, ip = sock.accept()
+    nome = conn.recv(1024)
+    lista_jogadores.append(jogador(nome, conn, ip))
+    print(lista_jogadores[i].nome.decode() + " se conectou.")
+
+palavra = input("Escolha a palavra: ").lower()
+
+#SETANDO AS CONFIGURAÇÕES DE CADA JOGADOR
+branco = ''
+for i in range(len(palavra)):
+    branco+='_'
+
+for i in range(num_jogadores):
+    lista_jogadores[i].palavra = branco
+    lista_jogadores[i].pontos = 6;
+    
+   
+for j in range(num_jogadores):
+    time.sleep(0.001)
+    lista_jogadores[j].conn.send(branco.encode())  # MANDANDO O BRANCO
+
+fim = 1
+limpar()
+
+while(fim):
+    for i in range(num_jogadores):
+        placar ="Placar: \n"
+        for jog in range(num_jogadores): #MONTANDO O PLACAR
+            placar+= lista_jogadores[jog].nome.decode() + '\t' + str(lista_jogadores[jog].pontos) + '\n'
+
+        print(placar)
+
+        for j in range(num_jogadores):
+            time.sleep(0.001)
+            lista_jogadores[j].conn.send(placar.encode()) #MANDANDO O PLACAR PARA TODOS OS JOGADORES
+            nada = lista_jogadores[j].conn.recv(1024)
+            if(j != i):
+                time.sleep(0.001)
+                lista_jogadores[j].conn.send(lista_jogadores[i].nome.decode() + " está jogando.") #MANDANDO PARA OS JOGADORES QUEM ESTÁ JOGANDO
+
+        print(lista_jogadores[i].nome.decode() + " está jogando.")
+
+        time.sleep(0.001)
+        lista_jogadores[i].conn.send("É a sua vez de jogar: ".encode()) #MANDANDO PARA O JOGADOR QUE É SUA VEZ DE JOGAR
+        ret = lista_jogadores[i].conn.recv(1024)
+        if(len(ret) == 1):
+            perdepontos = 1
+            novapalavra = ''
+            for c in range(len(palavra)):
+                if(palavra[c] == ret):
+                    perdepontos = 0
+                    novapalavra += ret
+                else:
+                    novapalavra += lista_jogadores[i].palavra[c]
+
+
+            lista_jogadores[i].palavra = novapalavra
+            lista_jogadores[i].pontos -= perdepontos
+            time.sleep(0.001)
+            lista_jogadores[i].conn.send(lista_jogadores[i].palavra.encode())
+            checarpontos(i)
+        else:
+            perdepontos = 2
+            if(palavra == ret):
+                fim = 0
+                lista_jogadores[i].palavra = palavra
+                lista_vencedores.append(lista_jogadores[i])
+                perdepontos = 0
+            lista_jogadores[i].pontos -= perdepontos
+            time.sleep(0.001)
+            lista_jogadores[i].conn.send(lista_jogadores[i].palavra.encode())
+            checarpontos(i)
+
+        for j in range(num_jogadores):
+            if(j != i):
+                time.sleep(0.001)
+                lista_jogadores[j].conn.send("Fim de turno") #MANDANDO PARA OS JOGADORES QUE ESTAVAM ESPERANDO QUE O TURNO ACABOU
+
+#Determinar um o vencedor
+maior = max(i.pontos for i in lista_vencedores)
+vencedor = ''
+
+for i in range(len(lista_vencedores)):
+    if(lista_vencedores[i].pontos == maior):
+        vencedor += '|' + lista_vencedores[i].nome.decode()
+
+
+#Mandando o sinal que o jogo acabou
+for i in range(num_jogadores):
+    lista_jogadores[i].conn.send("fim de jogo"+vencedor.encode())
+
+sock.close()
+
